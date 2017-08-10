d74f1dc26d5e1ea6d81f1b52109af34c461fc8df
Soetji Anto <santo@galileosuite.com>
2017-08-10 11:12:10 -0400

    Add html title

--- charts-redraw/test.js
+++ charts-redraw/test.js
@@ -1,9 +1,9 @@
 const dataUrl = 'http://g01dlapp01.galileosuite.com:3000/charts-redraw';
 const saveData = false;
 const chartsTotal = getUrlValue();
-const dataPointsTotal = 300;
+const dataPointsTotal = 900;
 const seriesTotal = 200;
-const chartsMax = 30;
+const chartsMax = 15;
 
 const initialData = {
     date: new Date(),
